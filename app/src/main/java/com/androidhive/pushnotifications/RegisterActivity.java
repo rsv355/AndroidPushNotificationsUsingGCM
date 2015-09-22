84706ccc622b015ed7ec91a01303da0ca090ca06
Krishna <softeng.krishna@gmail.com>
2015-09-23 01:39:58 +0530

    Initial commit

---
+++ app/src/main/java/com/androidhive/pushnotifications/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.androidhive.pushnotifications;
+
+import static com.androidhive.pushnotifications.CommonUtilities.SENDER_ID;
+import static com.androidhive.pushnotifications.CommonUtilities.SERVER_URL;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends Activity {
+	// alert dialog manager
+	AlertDialogManager alert = new AlertDialogManager();
+	
+	// Internet detector
+	ConnectionDetector cd;
+	
+	// UI elements
+	EditText txtName;
+	EditText txtEmail;
+	
+	// Register button
+	Button btnRegister;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+		cd = new ConnectionDetector(getApplicationContext());
+
+		// Check if Internet present
+		if (!cd.isConnectingToInternet()) {
+			// Internet Connection is not present
+			alert.showAlertDialog(RegisterActivity.this,
+					"Internet Connection Error",
+					"Please connect to working Internet connection", false);
+			// stop executing code by return
+			return;
+		}
+
+		// Check if GCM configuration is set
+		if (SERVER_URL == null || SENDER_ID == null || SERVER_URL.length() == 0
+				|| SENDER_ID.length() == 0) {
+			// GCM sernder id / server url is missing
+			alert.showAlertDialog(RegisterActivity.this, "Configuration Error!",
+					"Please set your Server URL and GCM Sender ID", false);
+			// stop executing code by return
+			 return;
+		}
+		
+		txtName = (EditText) findViewById(R.id.txtName);
+		txtEmail = (EditText) findViewById(R.id.txtEmail);
+		btnRegister = (Button) findViewById(R.id.btnRegister);
+		
+		/*
+		 * Click event on Register button
+		 * */
+		btnRegister.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// Read EditText dat
+				String android_id = Settings.Secure.getString(getContentResolver(),
+						Settings.Secure.ANDROID_ID);
+				
+				// Check if user filled the form
+
+					// Launch Main Activity
+					Intent i = new Intent(getApplicationContext(), MainActivity.class);
+					
+					// Registering user on our server					
+					// Sending registraiton details to MainActivity
+					i.putExtra("android_id", android_id);
+					startActivity(i);
+					finish();
+
+			}
+		});
+	}
+
+}
